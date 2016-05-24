5f65942360b3d68d5388003defcc3c5e31bce5d5
rbarrientos182 <barrientos.isc@gmail.com>
2016-05-24 13:07:12 -0500

    Initial commit

---
+++ app/src/main/java/com/android/rbarrientos/needer/MainInicioSesion.java
@@ -0,0 +1,44 @@
+package com.android.rbarrientos.needer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainInicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_inicio_sesion);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void irInicio(View v) {
+        Intent intent = new Intent();
+        intent.setClass(MainInicioSesion.this, MainActivity.class);
+        startActivity(intent);
+        finish(); // finaliza la actividad "Registro"
+    }
+
+    public void irBienvenido(View v) {
+        Intent intent = new Intent();
+        intent.setClass(MainInicioSesion.this, MainBienvenida.class);
+        startActivity(intent);
+        finish(); // finaliza la actividad "Registro"
+    }
+
+}
